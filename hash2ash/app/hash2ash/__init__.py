ef650ef3f5aabbb232fff8770dd149f548c592ec
reda aitfaska <redaaitfaska7@gmail.com>
2024-06-13 13:26:22 +0200

    :art: improve init.py structure

--- hash2ash/app/hash2ash/__init__.py
+++ hash2ash/app/hash2ash/__init__.py
@@ -6,14 +6,20 @@ from flask_login import LoginManager
 from flask_mail import Mail
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-app.config['SECURITY_PASSWORD_SALT'] = os.getenv("SECURITY_PASSWORD_SALT")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+mail = Mail(app)
+
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config['SECURITY_PASSWORD_SALT'] = os.getenv("SECURITY_PASSWORD_SALT")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+
+#db.init_app(app) # Initialize the database
+
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
 app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
@@ -21,6 +27,6 @@ app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_ASCII_ATTACHMENTS'] = False
 app.config['MAIL_DEFAULT_CHARSET'] = 'utf-8'
-mail = Mail(app)
+
 
 from hash2ash import routes
