ce441d1b02224e0c77a1da3da04c97657475c0de
Caaams95 <camillelctmlc@gmail.com>
2024-06-26 19:30:57 +0200

    backup NOTOK

--- terraform_new_instance.sh
+++ terraform_new_instance.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+status_processing="Processing"
+
 # Vérifier si l'argument du chemin du fichier est fourni
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <path_to_script>"
@@ -19,10 +21,8 @@ new_count=$((current_count + 1))
 # Modifier la configuration Terraform pour ajouter une nouvelle instance
 terraform apply -auto-approve -var="instance_count=$new_count" -var="script_path=$script_path"
 
-
 # Récupérer les détails des instances créées
 instances_details=$(terraform output -json instances_details)
-
 
 # Pour chaque nouvelle instance créée
 for instance_detail in $(echo "${instances_details}" | jq -c '.[]'); do
@@ -31,24 +31,22 @@ for instance_detail in $(echo "${instances_details}" | jq -c '.[]'); do
     instance_name=$(echo "${instance_detail}" | jq -r '.name')
     instance_id=$(echo "${instance_detail}" | jq -r '.instance_id')
 
+    # Insérer l'ID de l'instance dans la base de données (exemple avec PostgreSQL)
+    is_processed=$(PGPASSWORD="A72gm143kldF47GI" psql -h hash2ash.c3m2i44y2jm0.us-east-1.rds.amazonaws.com -U userHash2ash -d initial_db -p 5432 -t -c "SELECT COUNT(*) FROM information_schema.instance WHERE instance_id = '$instance_id';")
 
-    is_processed=$(PGPASSWORD="A72gm143kldF47GI" psql -h hash2ash.c3m2i44y2jm0.us-east-1.rds.amazonaws.com -U userHash2ash -d initial_db -p 5432 -t -c "SELECT COUNT(*) FROM information_schema.instance WHERE instance_id = '$instance_id';")    # Insérer l'ID de l'instance dans la base de données (exemple avec PostgreSQL)
-    
     if [ "$is_processed" -le 0 ]; then
-        # Exécuter hashcat en arrière-plan sur l'instance créée
+    # Ajouter l'instance dans la base de données
+        PGPASSWORD="A72gm143kldF47GI" psql -h hash2ash.c3m2i44y2jm0.us-east-1.rds.amazonaws.com -U userHash2ash -d initial_db -p 5432 -c "INSERT INTO information_schema.instance (instance_id, instance_name, instance_ip, status) VALUES ('$instance_id', '$instance_name', '$instance_ip', '$status_processing');"
+
+        # Exécuter des commandes sur l'instance créée (exemple avec SSH)
         ssh -o "StrictHostKeyChecking=no" -i /home/cams/.ssh/Cle_test_terraform.pem ubuntu@"$instance_ip" \
             'sudo apt-get update -y && sudo apt-get upgrade -y && \
             sudo apt install postgresql -y && \
             sudo apt-get install hashcat -y && \
             chmod +x /tmp/script.sh && \
-            /tmp/script.sh > /tmp/hash2ash.log 2>&1'
-        
-        # AJouter l'instance dans la BDD
-        PGPASSWORD="A72gm143kldF47GI" psql -h hash2ash.c3m2i44y2jm0.us-east-1.rds.amazonaws.com -U userHash2ash -d initial_db -p 5432 -c "INSERT INTO information_schema.instance (instance_id, instance_name, instance_ip, status) VALUES ('$instance_id', '$instance_name', '$instance_ip', 1);"
+            /tmp/script.sh'
 
-        # Afficher un message indiquant que le bruteforce a démarré sur l'instance
-        echo "Bruteforce démarré en arrière-plan sur l'instance $instance_name avec l'adresse IP $instance_ip"
-    fi
+            fi
 done
 
 # Afficher le succès de la création de l'instance finale
--- terraform_stop_instance.sh
+++ terraform_stop_instance.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
-# Vérifier si l'argument du chemin du fichier est fourni
+# Vérifiez si l'ID de l'instance est passé en argument
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <instance_id>"
     exit 1
 fi
 
-# Instance ID à supprimer
+# ID de l'instance à supprimer
 instance_id=$1
 
+# Obtenir la liste des instances gérées par Terraform
+terraform_instances=$(terraform state list | grep 'aws_instance.instance-gratuite')
 
-# Trouver l'index de l'instance dans l'état Terraform
-instance_index=$(terraform state list | grep 'aws_instance.instance-gratuite' | grep -o '\[.*\]' | tr -d '[]')
-
-# Si l'instance existe dans l'état Terraform, la supprimer de l'état
-if [ -n "$instance_index" ]; then
-  terraform state rm "aws_instance.instance-gratuite[$instance_index]"
-fi
+# Trouver l'index de l'instance dans l'état Terraform par son ID AWS
+for instance in $terraform_instances; do
+  current_id=$(terraform state show $instance | grep "id                                   =" | awk '{print $3}' | tr -d '"')
+  echo "$instance_id == $current_id"
+  if [ "$current_id" == "$instance_id" ]; then
+    # Supprimer l'instance de l'état Terraform
+    terraform state rm $instance
+    break
+  fi
+done
 
 # Terminer l'instance avec AWS CLI
+echo "aws ec2 terminate-instances --instance-ids $instance_id"
 aws ec2 terminate-instances --instance-ids "$instance_id"
 
-# Appliquer la configuration Terraform pour valider les changements
-terraform apply -auto-approve
+# Mettre à jour le statut dans la base de données
+status_shutdown="Shutdown"
+PGPASSWORD='A72gm143kldF47GI' psql -U userHash2ash -h hash2ash.c3m2i44y2jm0.us-east-1.rds.amazonaws.com -p 5432 -d initial_db -c "UPDATE information_schema.instance SET status='$status_shutdown' WHERE instance_id='$instance_id';"
+PGPASSWORD='A72gm143kldF47GI' psql -U userHash2ash -h hash2ash.c3m2i44y2jm0.us-east-1.rds.amazonaws.com -p 5432 -d initial_db -c "UPDATE information_schema.instance SET date_shutdown=now() WHERE instance_id='$instance_id';"
 
-PGPASSWORD='A72gm143kldF47GI' psql -U userHash2ash -h hash2ash.c3m2i44y2jm0.us-east-1.rds.amazonaws.com -p 5432 -d initial_db -c "UPDATE information_schema.instance SET status='0' WHERE instance_id=$instance_id;"
+# Afficher le succès de la suppression de l'instance
+echo "Instance supprimée avec succès : $instance_id"
