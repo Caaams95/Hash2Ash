15a1de9cdf441059b6ebe2ef93d5af74753e6807
Caaams95 <camillelctmlc@gmail.com>
2024-06-27 02:56:27 +0200

    feat: No psswd found OK , TODO: react if wrong in scrtip.sh + DB new_instance

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DB_USERNAME="userHash2ash"
-DB_PASSWORD=" "
-DB_DNS="hash2ash.c3m2i44y2jm0.us-east-1.rds.amazonaws.com"
+DB_PASSWORD="A72gm143kldF47GI"
+DB_HOST="hash2ash.c3m2i44y2jm0.us-east-1.rds.amazonaws.com"
 DB_PORT="5432"
 DB_NAME="initial_db"
 
@@ -10,16 +10,25 @@ DB_TABLE="information_schema.hashes"
 echo "Processing" > /tmp/status.txt
 
 hashcat -m 400 /tmp/hash.hash /tmp/example.dict --status -O
-hashcat -m 400 /tmp/hash.hash /tmp/example.dict --show
-hashcat -m 400 /tmp/hash.hash /tmp/example.dict --show > /tmp/result.txt
+HASHCAT_EXIT_CODE=$?
 
-# Lire le fichier de sortie de Hashcat
-while IFS=: read -r hash password
-do
-    if [ -n $"hash" ] && [ -n "$password" ]; then
-        # Mettre à jour la base de données en une seule ligne
-        echo "UPDATE $DB_TABLE SET result='$password' WHERE id_hash=(SELECT MAX(id_hash) FROM $DB_TABLE);" > /tmp/commande.sql
-        PGPASSWORD="$DB_PASSWORD" psql -U $DB_USERNAME -h $DB_DNS -p $DB_PORT -d $DB_NAME -f /tmp/commande.sql
-        echo "Finish" > /tmp/status.txt
-        fi
-done < /tmp/result.txt
+# Vérifie le code de sortie de hashcat
+if [ $HASHCAT_EXIT_CODE -ne 0 ]; then
+    echo "Password pas trouvé"
+else
+    # password trouvé
+    hashcat -m 400 /tmp/hash.hash /tmp/example.dict --show
+    hashcat -m 400 /tmp/hash.hash /tmp/example.dict --show > /tmp/result.txt
+
+    # Lire le fichier de sortie de Hashcat
+    while IFS=: read -r hash password
+    do
+        if [ -n $"hash" ] && [ -n "$password" ]; then
+            # Mettre à jour la base de données en une seule ligne
+            echo "UPDATE $DB_TABLE SET result='$password' WHERE id_hash=(SELECT MAX(id_hash) FROM $DB_TABLE);" > /tmp/commande.sql
+            PGPASSWORD="$DB_PASSWORD" psql -U $DB_USERNAME -h $DB_HOST -p $DB_PORT -d $DB_NAME -f /tmp/commande.sql
+            echo "Finish" > /tmp/status.txt
+            fi
+    done < /tmp/result.txt
+
+fi
