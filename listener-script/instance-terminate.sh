e5b3348d4268f6984f25e966c740080088577de7
Caaams95 <camillelctmlc@gmail.com>
2024-06-28 23:24:03 +0200

    feat: Listener OK

--- listener-script/instance-terminate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <id_instance>"
-    exit 1
-fi
-
-instance_id=$1
-PGPASSWORD='C5yAn39f8Tm7U13z' psql -U userHash2ash -h db-hash2ash-prod.c3m2i44y2jm0.us-east-1.rds.amazonaws.com -p 5432 -d hash2ash -c "UPDATE public.instances SET status='Terminate' WHERE instance_id='$instance_id';"
