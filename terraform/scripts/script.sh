4c96edce2d9a78c6eb2fa7b9ab2a4c02d58cda8d
Caaams95 <camillelctmlc@gmail.com>
2024-06-29 13:30:40 +0200

    feat: script.sh <id_hash> almost ready

--- terraform/scripts/script.sh
+++ terraform/scripts/script.sh
@@ -7,7 +7,17 @@ DB_NAME="hash2ash"
 
 TABLE_HASHES="public.hashes"
 
+if [ $# -ne  ]; then
+    echo "Usage: $0 <id_arch>"
+    exit 1
+fi
 
+id_arch=$1
+id_hash=$2
+
+# donne le fk_id_instance au hash
+id_instance=$(PGPASSWORD='C5yAn39f8Tm7U13z' psql -U userHash2ash -h db-hash2ash-prod.c3m2i44y2jm0.us-east-1.rds.amazonaws.com -p 5432 -d hash2ash -t -c "SELECT id_instance FROM public.instances WHERE id_arch = '$id_arch';" | tr -d '[:space:]')
+PGPASSWORD='C5yAn39f8Tm7U13z' psql -U userHash2ash -h db-hash2ash-prod.c3m2i44y2jm0.us-east-1.rds.amazonaws.com -p 5432 -d hash2ash -c "UPDATE public.hashes SET fk_id_instance=$id_instance WHERE id_hash='$id_hash';"
 
 hashcat -m 400 /tmp/hash.hash /tmp/example.dict --status -O
 # hashcat is processing, wait...
@@ -20,7 +30,8 @@ HASHCAT_EXIT_CODE=$?
 
 # Vérifie le code de sortie de hashcat
 if [ $HASHCAT_EXIT_CODE -ne 0 ]; then
-    PGPASSWORD="$DB_PASSWORD" psql -U $DB_USERNAME -h $DB_HOST -p $DB_PORT -d $DB_NAME -c "UPDATE publilc.hashes SET status='Not Found' WHERE id_hash=(SELECT MAX(id_hash) FROM $TABLE_HASHES);"
+    # password pas trouvé
+    PGPASSWORD="$DB_PASSWORD" psql -U $DB_USERNAME -h $DB_HOST -p $DB_PORT -d $DB_NAME -c "UPDATE $TABLE_HASHES SET status='Not Found' WHERE id_hash=$id_hash ;"
 else
     # password trouvé
     hashcat -m 400 /tmp/hash.hash /tmp/example.dict --show
@@ -32,8 +43,9 @@ else
     do
         if [ -n "$hash" ] && [ -n "$password" ]; then
             # Mettre à jour la base de données en une seule ligne
-            PGPASSWORD="$DB_PASSWORD" psql -U $DB_USERNAME -h $DB_HOST -p $DB_PORT -d $DB_NAME -c "UPDATE $TABLE_HASHES SET result='$password' WHERE id_hash=(SELECT MAX(id_hash) FROM $TABLE_HASHES);"
+            PGPASSWORD="$DB_PASSWORD" psql -U $DB_USERNAME -h $DB_HOST -p $DB_PORT -d $DB_NAME -c "UPDATE $TABLE_HASHES SET result='$password' WHERE id_hash=$id_hash;"
             fi
     done < /tmp/result.txt
 
 fi
+
