020fdc95132e44218aa1cabb397452a67a44703e
Caaams95 <camillelctmlc@gmail.com>
2024-07-03 00:54:05 +0200

    rename folder

--- app-web/src/forms.py
+++ app-web/src/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from flask_login import current_user
 from wtforms import StringField , PasswordField, SubmitField, BooleanField, SelectField, FileField, SelectMultipleField, widgets, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Optional
-from hash2ash.models import Users, Instances, Hashes
+from src.models import Users, Instances, Hashes
 from flask_wtf.file import FileAllowed, FileRequired
 
 HASHCAT_ALGORITHMS = [
