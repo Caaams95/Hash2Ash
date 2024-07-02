b68e504b3b553da662bcc49240ed61b5e2878dad
Caaams95 <92918705+Caaams95@users.noreply.github.com>
2024-07-02 17:43:51 +0200

    Clean hash-status.sh

--- terraform/hash-status.sh
+++ terraform/hash-status.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+source ../.env
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <id_instance>"
     exit 1
@@ -7,5 +7,5 @@ fi
 
 id_arch=$1
 status=$2
-PGPASSWORD='C5yAn39f8Tm7U13z' psql -U userHash2ash -h db-hash2ash-prod.c3m2i44y2jm0.us-east-1.rds.amazonaws.com -p 5432 -d hash2ash -c "UPDATE public.hashes SET status = '$status' FROM public.instances WHERE public.instances.id_instance = public.hashes.fk_id_instance AND public.instances.id_instance = (SELECT id_instance FROM public.instances WHERE id_arch = '$id_arch');"
+PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USERNAME" -h "$DB_HOST" -p "$DB_PORT" -d "$DB_NAME" -c "UPDATE public.hashes SET status = '$status' FROM public.instances WHERE public.instances.id_instance = public.hashes.fk_id_instance AND public.instances.id_instance = (SELECT id_instance FROM public.instances WHERE id_arch = '$id_arch');"
 
